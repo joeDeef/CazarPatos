178e3f37fc21739086d1a14e815f1a4f5b5c57a0
DESKTOP-TOD8GSC <DESKTOP-TOD8GSC@epn.edu.ec>
2025-05-16 15:42:18 -0500

    Navigating between screens

---
+++ app/src/main/java/com/defaz/joel/cazarpatos/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.defaz.joel.cazarpatos
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var ButtonLoggin: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ButtonLoggin = findViewById(R.id.buttonLoggin)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        ButtonLoggin.setOnClickListener {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
