401f262590b14c94d8967819c7e8d9b91488d2d2
DESKTOP-TOD8GSC <DESKTOP-TOD8GSC@epn.edu.ec>
2025-06-27 15:47:18 -0500

    Read/Write files funtions with SharedPreferences, EncriptedShared, FileInternal, FileExternal

--- app/src/main/java/com/defaz/joel/cazarpatos/Constant.kt
+++ app/src/main/java/com/defaz/joel/cazarpatos/Constant.kt
@@ -1,3 +1,6 @@
 package com.defaz.joel.cazarpatos
 
 const val EXTRA_LOGIN = "EXTRA_LOGIN"
+const val LOGIN_KEY = "LOGIN_KEY"
+const val PASSWORD_KEY = "PASSWORD_KEY"
+const val SHAREDINFO_FILENAME = "mySharedInformation.dat"
--- app/src/main/java/com/defaz/joel/cazarpatos/LoginActivity.kt
+++ app/src/main/java/com/defaz/joel/cazarpatos/LoginActivity.kt
@@ -4,30 +4,33 @@ import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var manejadorArchivo: FileHandler
     lateinit var editTextEmail: EditText
     lateinit var editTextPassword:EditText
     lateinit var buttonLogin: Button
     lateinit var buttonNewUser:Button
+    lateinit var checkBoxRecordarme: CheckBox
     lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }*/
         //Inicialización de variables
+        //manejadorArchivo = SharedPreferencesManager(this)
+        manejadorArchivo = FileExternalManager(this)
         editTextEmail = findViewById(R.id.editTextEmail)
         editTextPassword = findViewById(R.id.editTextPassword)
         buttonLogin = findViewById(R.id.buttonLogin)
         buttonNewUser = findViewById(R.id.buttonNewUser)
+        checkBoxRecordarme = findViewById(R.id.checkBoxRecordarme)
+
+        LeerDatosDePreferencias()
         //Eventos clic
         buttonLogin.setOnClickListener {
             val email = editTextEmail.text.toString()
@@ -35,6 +38,7 @@ class LoginActivity : AppCompatActivity() {
             //Validaciones de datos requeridos y formatos
             if(!validateRequiredData())
                 return@setOnClickListener
+            GuardarDatosEnPreferencias()
             //Si pasa validación de datos requeridos, ir a pantalla principal
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtra(EXTRA_LOGIN, email)
@@ -47,6 +51,29 @@ class LoginActivity : AppCompatActivity() {
         mediaPlayer=MediaPlayer.create(this, R.raw.title_screen)
         mediaPlayer.start()
     }
+
+    private fun LeerDatosDePreferencias(){
+        val listadoLeido = manejadorArchivo.ReadInformation()
+        if(listadoLeido.first != null){
+            checkBoxRecordarme.isChecked = true
+        }
+        editTextEmail.setText ( listadoLeido.first )
+        editTextPassword.setText ( listadoLeido.second )
+    }
+
+    private fun GuardarDatosEnPreferencias(){
+        val email = editTextEmail.text.toString()
+        val clave = editTextPassword.text.toString()
+        val listadoAGrabar:Pair<String,String>
+        if(checkBoxRecordarme.isChecked){
+            listadoAGrabar = email to clave
+        }
+        else{
+            listadoAGrabar ="" to ""
+        }
+        manejadorArchivo.SaveInformation(listadoAGrabar)
+    }
+
     private fun validateRequiredData():Boolean{
         val email = editTextEmail.text.toString()
         val password = editTextPassword.text.toString()
