94902cf727e06b5e87eede9b0e060da01fcec722
Joel Defaz <joe.def2019@gmail.com>
2025-07-03 15:38:40 -0500

    Lista de jugadores

---
+++ app/src/main/java/com/defaz/joel/cazarpatos/Player.kt
@@ -0,0 +1,5 @@
+package com.defaz.joel.cazarpatos
+
+data class Player (var username:String, var huntedDucks:Int){
+    constructor():this("",0)
+}
---
+++ app/src/main/java/com/defaz/joel/cazarpatos/RankingActivity.kt
@@ -0,0 +1,35 @@
+package com.defaz.joel.cazarpatos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class RankingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ranking)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+        var jugadores = arrayListOf<Player>()
+        jugadores.add(Player("Joel Defaz",10))
+        jugadores.add(Player("Elver Calisto",6))
+        jugadores.add(Player("Susana Horia",3))
+        jugadores.add(Player("Alan Brito",9))
+        jugadores.add(Player("Estefi",9))
+        jugadores.add(Player("Carlos",9))
+        jugadores.sortByDescending { it.huntedDucks }
+
+        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+        recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+        recyclerViewRanking.adapter = RankingAdapter(jugadores);
+        recyclerViewRanking.setHasFixedSize(true);
+    }
+}
